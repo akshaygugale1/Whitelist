0d331f2f60ef3cd17dc84acc5d8e38b5ea354844
Gugale <akshay.gugale@accenture.com>
2022-06-09 02:56:31 +0530

    Updated testnetwork to Mumbai

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,15 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: ".env" });
 
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_URL;
+const ALCHEMY_RINKEBY_API_KEY_URL = process.env.ALCHEMY_RINKEBY_URL;
+const ALCHEMY_MUMBAI_API_KEY_URL = process.env.ALCHEMY_MUMBAI_URL;
 
-const RINKEBY_PRIVATE_KEY = process.env.ACCOOUNT;
+const PRIVATE_KEY = process.env.ACCOOUNT;
 module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: ALCHEMY_API_KEY_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
+      url: ALCHEMY_RINKEBY_API_KEY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    mumbai: {
+      url: ALCHEMY_MUMBAI_API_KEY_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
